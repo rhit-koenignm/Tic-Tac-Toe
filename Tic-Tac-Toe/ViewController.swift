388c09bf1439a4e62c26da3e9f1f3edebf929ff6
Natalie Koenig <koenignm@rose-hulman.edu>
2021-06-25 00:00:20 -0500

    Started implementation

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameStateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressedGameBoardButton(_ sender: Any) {
+    }
+    
+    @IBAction func pressedNewGame(_ sender: Any) {
+        print("New Game")
+    }
+    
 }
 
