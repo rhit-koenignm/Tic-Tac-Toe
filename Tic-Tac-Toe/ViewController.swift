eff388dda5ed71a3bb863f743c0d8ba8b4dc541e
Natalie Koenig <koenignm@rose-hulman.edu>
2021-06-25 08:51:47 -0500

    Done with this follow-along

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -11,16 +11,69 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var gameStateLabel: UILabel!
     
+    @IBOutlet var gameBoardButtons: [UIButton]!
+    
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    
+    @IBOutlet weak var gameBoardImage: UIImageView!
+    var game = TicTacToeGame()
+    
+    var xImage: UIImage!
+    var oImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact {
+            gameBoardImage.image = #imageLiteral(resourceName: "iPhone_board.png")
+            xImage = #imageLiteral(resourceName: "iPhone_X.png")
+            oImage = #imageLiteral(resourceName: "iPhone_O.png")
+        } else {
+            gameBoardImage.image = #imageLiteral(resourceName: "iPad_board.png")
+            xImage = #imageLiteral(resourceName: "iPad_X.png")
+            oImage = #imageLiteral(resourceName: "iPad_O.png")
+        }
     }
 
     @IBAction func pressedGameBoardButton(_ sender: Any) {
+        let button = sender as! UIButton
+        print(button.tag)
+        game.pressedSquareAt(button.tag)
+        updateView()
     }
+    
     
     @IBAction func pressedNewGame(_ sender: Any) {
         print("New Game")
+        game = TicTacToeGame()
+        
+        updateView()
+    }
+    
+    func updateView() {
+        print("Game state: \(game)")
+        
+        if(traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact) {
+            // iPhone
+            gameStateLabel.text = game.state.rawValue
+        }
+        else {
+            // iPad
+            gameStateNavBar.topItem?.title = game.state.rawValue
+        }
+        
+        for button in gameBoardButtons {
+            let buttonIndex = button.tag
+            
+            switch game.board[buttonIndex]{
+            case .none:
+                button.setImage(nil, for: UIControl.State.normal)
+            case .x:
+                button.setImage(xImage, for: .normal)
+            case .o:
+                button.setImage(oImage, for: .normal)
+            }
+        }
     }
     
 }
